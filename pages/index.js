1b43ae97a1c643e60ee8b853b57d58f6c2b45b49
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-24 22:57:13 +0530

    added : getStaticProps basic intro with dummy data

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,34 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home(props) {
+  const { products } = props;
   return (
     <>
-      <h1>Hello World</h1>
+      <ul>
+        {products?.map((product) => {
+          return <li key={product.id}>{product.title}</li>;
+        })}
+      </ul>
     </>
   );
+}
+
+// this function prepares the props for the component at the time of pre-rendering.
+
+// Next js first executes this function and in second step execute component function.
+
+// Any file that is containig getStaticProps function is pre-rendered.
+
+// Code inside getStaticProps is not visible on client side/ not sent to client side.
+export async function getStaticProps() {
+  // It fetches data and exposes the data to the componet using props.
+
+  // Always returns an object with a "props" key
+  // All of this happens at the build time.
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
 }
