94ebf1b98dff0ef99415e252b0ab338bdbf0eed7
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-31 22:40:29 +0530

    Combining pre-fetching with client-side fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const LastSales = () => {
-  const [sales, setSales] = useState();
+const LastSales = (props) => {
+  const [sales, setSales] = useState(props.sales);
   const [isLoading, setIsLoading] = useState(false);
 
   // swr is a hook that we can use to make client side requests.
@@ -79,5 +79,34 @@ const LastSales = () => {
     </div>
   );
 };
+
+// pre-generate the data during the built process and possibly revalidate it after deployment
+// with revalidate key
+
+// now the initial load or pre-rendered page will contain the data that is generated at built and after request
+// is sent to server because of ISR
+
+export async function getStaticProps() {
+  return fetch("firebase-api-route")
+    .then((response) => response.json())
+    .then((data) => {
+      const trasformedData = [];
+
+      for (const key in data) {
+        trasformedData.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      return {
+        props: {
+          sales: trasformedData,
+        },
+        revalidate: 10,
+      };
+    });
+}
 
 export default LastSales;
