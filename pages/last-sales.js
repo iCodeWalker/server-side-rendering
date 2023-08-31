8950e8b10f4230300f858f1e159764dfa253896f
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-31 22:21:17 +0530

    added : SWR hook for client side data fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,35 +1,72 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const LastSales = () => {
   const [sales, setSales] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
+  // swr is a hook that we can use to make client side requests.
+  // it provides some built in data and error handling
+
+  // It also bundles multiple requests and send a single request to limit multiple request to server
+  const { data, error } = useSWR("firebase-api-route");
+
   useEffect(() => {
-    setIsLoading(true);
-    fetch("firebase-api-route")
-      .then((response) => response.json())
-      .then((data) => {
-        const trasformedData = [];
+    if (data) {
+      const trasformedData = [];
 
-        for (const key in data) {
-          trasformedData.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(trasformedData);
-        setIsLoading(false);
-      });
-  }, []);
+      for (const key in data) {
+        trasformedData.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(trasformedData);
+    }
+  }, [data]);
 
-  if (isLoading) {
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("firebase-api-route")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const trasformedData = [];
+
+  //         for (const key in data) {
+  //           trasformedData.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(trasformedData);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  //   if (isLoading) {
+  //     return <p>Loading...</p>;
+  //   }
+
+  //----->> error is also undefined for initial load that's why this is also not used in pre-rendering
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+
+  //----->> this is used for pre-rendering the page
+  if (!data) {
     return <p>Loading...</p>;
   }
 
   if (!sales) {
-    return <p>No data yet</p>;
+    return <p>Loading...</p>;
   }
+
+  //   if (!sales) {
+  //     return <p>No data yet</p>;
+  //   }
+
   return (
     <div>
       <ul>
